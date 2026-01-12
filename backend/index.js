d8023a72a9ce73cb4ffdd638503cacd19ce58342
Patel purv <patelpurv2643@gmail.com>
2026-01-12 21:15:43 +0530

    razorpay controller added

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import cinemaRoutes from "./routes/cinemaRoutes.js";
 import showRoutes from "./routes/showRoutes.js";
 import ticketRoutes from "./routes/ticketRoutes.js";
 import movieLikeRoutes from "./routes/movieLikeRoutes.js";
+import paymentRoutes from './routes/paymentRoutes.js'
 import connectDB from "./config/db.js";
 
 const app = express();
@@ -26,28 +27,13 @@ app.use(express.json());
 // connect to db
 connectDB();
 
-//seed cinema data
-// const seedCinemas = async () => {
-//     try {
-//         const count = await Cinema.countDocuments();
-//         if (count === 0) {
-//             await Cinema.insertMany(cinemaData);
-//             console.log("Cinema data seeded");
-//         }
-
-//     } catch (error) {
-//         console.log("Error seeding cinema data:", error.message);
-//     }
-// };
-
-// seedCinemas();
-
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);
 app.use("/api/cinemas", cinemaRoutes);
 app.use("/api/shows", showRoutes);
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/movieLikes", movieLikeRoutes);
+app.use("/api/payment",paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
