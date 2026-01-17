7f7b6fc0c5eddbf11b0f524c1f94c22be1d5d8b2
Patel purv <patelpurv2643@gmail.com>
2026-01-17 16:34:46 +0530

    getShowBy movie id added

--- backend/controllers/cinemaController.js
+++ backend/controllers/cinemaController.js
@@ -165,4 +165,3 @@ export const deleteCinema = async (req, res) => {
     }
 };
 
-
--- backend/routes/showRoutes.js
+++ backend/routes/showRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createShow, getAllShows, getShowById, getAllShowsAdmin, updateShow, cancelShow } from "../controllers/showController.js"
+import { createShow, getAllShows, getShowById, getAllShowsAdmin, updateShow, cancelShow ,getShowsByMovieAndDate} from "../controllers/showController.js"
 const router = express.Router();
 
 router.post("/", createShow);
@@ -8,6 +8,7 @@ router.get("/", getAllShows);
 router.get("/:id", getShowById);
 router.put("/:id", updateShow);
 router.put("/cancel/:id", cancelShow);
+router.get("/movie/:movieId", getShowsByMovieAndDate);
 
 
 
