11b324cdddc6c31a078d261193a7f3fb9a715db4
Patel purv <patelpurv2643@gmail.com>
2026-02-21 19:42:24 +0530

    feat: Add verification expiration handling to user model and update OTP verification process

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -117,6 +117,7 @@ export const verifyOtp = async (req, res) => {
         }
 
         user.isVerified = true;
+        user.verificationExpiresAt = undefined;
         await user.save();
 
         await Otp.deleteOne({ _id: otpRecord._id });
